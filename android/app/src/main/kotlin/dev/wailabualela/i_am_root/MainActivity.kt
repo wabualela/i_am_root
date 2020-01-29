cbb2df8a96a1a00296e440f0ada6633954376abe
wabualela <wailabualela@gmail.com>
2020-01-29 13:09:18 +0200

    initial i am root

---
+++ android/app/src/main/kotlin/dev/wailabualela/i_am_root/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.wailabualela.i_am_root
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
